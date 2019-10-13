ed53eac0e2602c2aa98f8a9f835bd79a98432e5d
juyong kim <Juyong.Kim@uon.edu.au>
2019-10-13 14:59:54 +1100

    finished commenting

--- StRec.java
+++ StRec.java
@@ -3,43 +3,44 @@
 //  * Assignment:               Assignment3
 //   * Name:                    Juyong Kim  
 //    * Student Number:         c3244203
-//     * Purpose:               Statement Requirement
-//      * Note:                 Look Below
+//     * Purpose:               Syntax Tree Requirements
+//      * Note:                 Pretty simple class
 
 public class StRec
 {
+	//variables
 	private String name;
 	private String type;
 
+	//constructor
 	public StRec()
 	{
 		name = null;
 		type = null;
 	}
 
+	//setters
 	public StRec(String name)
 	{
 		this();
 		this.name = name;
 	}
-
-	public String getName()
-	{
-		return name;
-	}
-
-	public String getType()
-	{
-		return type;
-	}
-
 	public void setName(String name)
 	{
 		this.name = name;
 	}
-
 	public void setType(String type)
 	{
 		this.type = type;
+	}
+
+	//getters
+	public String getName()
+	{
+		return name;
+	}
+	public String getType()
+	{
+		return type;
 	}
 }
